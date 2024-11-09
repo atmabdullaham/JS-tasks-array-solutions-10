35d0251dad61794c9b364317faa6bc15edd39ffb
atmabdullaham <atmabdullaham@gmail.com>
2024-11-10 04:58:20 +0600

    added the solutions for the problems three and four

--- task_03.js
+++ task_03.js
@@ -2,3 +2,6 @@
 // Create an array of books containing different books.
 // Use the includes method to check if the array contains a 'javascript' book.
 // Print a message to the console indicating whether the element is present in the array or not.
+const books = ['bangla', 'physics', 'english', 'javascript'];
+
+console.log(books.includes('javascript'));
--- task_04.js
+++ task_04.js
@@ -2,3 +2,13 @@
 // Create different variables, each containing either an array or a non-array value.
 // Use isArray to check if each variable is an array.
 // Print a message to the console indicating whether each variable is an array or not.
+
+
+const friends = ['Rakib', 'Sajid', 'Jabed', 'Tareq'];
+const nums = [15, 32, 54, 75];
+
+const sentence = 'My name is Mamun';
+
+console.log(Array.isArray(friends));
+console.log(Array.isArray(nums));
+console.log(Array.isArray(sentence));
