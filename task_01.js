a4c9dcd9cc4ad0694d7619ae9729d5460fb51ade
atmabdullaham <atmabdullaham@gmail.com>
2024-11-10 04:50:46 +0600

    make solutions for the task 03

---
+++ task_01.js
@@ -0,0 +1,34 @@
+// 1. Declare an Array
+// Declare an array with 5 elements containing fruits.
+// Console log the 3rd index element.
+// Change the value of the 2nd index element to 'jambura'.
+// Console log the final array.
+
+
+
+
+
+const fruits = ['🍍', '🍎', '🍏', '🍌', 'JackFruits'];
+console.log(fruits[3])
+
+fruits[2] = 'jambura';
+console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- task_02.js
+++ task_02.js
@@ -4,3 +4,14 @@
 // Add two more to your array.
 // Remove the last tourist destination you have added.
 // Display the final array as output.
+
+const touristSpot = ['CoxBazar', 'Saint-martin', 'Srimangle'];
+touristSpot.push('Sylet');
+console.log(touristSpot);
+touristSpot.push('rangpur', 'Hydrabad');
+
+console.log(touristSpot);
+
+touristSpot.pop()
+
+console.log(touristSpot);
